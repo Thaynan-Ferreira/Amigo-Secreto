db7deb8be54ecfb5f73b61d6f3a8179964f6fb21
Thaynan-Ferreira <thaynan_ferreira@outlook.com.br>
2024-07-25 10:06:17 -0300

    Implementando o Sortear

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 let amigos = [];
 function adicionar() {
     let amigoDigitado = document.querySelector('#nome-amigo').value;
-    let nomesSorteio = document.querySelector('#lista-amigos')
+    let nomesSorteio = document.querySelector('#lista-amigos');
     console.log(amigoDigitado);//teste
 
-    amigos.push(amigoDigitado)//incremento do array
-    console.log(amigos)//teste
+    amigos.push(amigoDigitado);//incremento do array
+    console.log(amigos);//teste
 
-    nomesSorteio.textContent = amigos
+    nomesSorteio.textContent = amigos;
     document.querySelector('#nome-amigo').value = '';//zerar input
     
 }
 
 function sortear(){
-    let sorteio = document.querySelector('.prizeDraw__container')
-    amigosEmbaralhados = embaralharArray(amigos)
-    console.log (amigosEmbaralhados)
+    let sorteio = document.querySelector('#lista-sorteio');
+    amigosEmbaralhados = embaralharArray(amigos);
+    console.log (amigosEmbaralhados);
+
+    for(let i = 0; i < amigosEmbaralhados.length; i++){
+        if(i == amigosEmbaralhados.length-1){
+            sorteio.innerHTML = sorteio.innerHTML + `${amigosEmbaralhados[i]} --> ${amigosEmbaralhados[0]} <br>`;
+        }else{
+            sorteio.innerHTML = sorteio.innerHTML + `${amigosEmbaralhados[i]} --> ${amigosEmbaralhados[i+1]} <br>`;
+        }
+
+        
+    }
+
 
     
 }
